106313bff1af90d9ecdcb38860e52f3e0bf90f00
endo1322 <bondroid3.22@icloud.com>
2024-02-10 13:31:15 +0900

    init: shadcn

--- src/app/[local]/page.tsx
+++ src/app/[local]/page.tsx
@@ -1,6 +1,12 @@
 import { useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
 
 export default function Index() {
   const t = useTranslations("Index");
-  return <h1>{t("title")}</h1>;
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <Button size={"sm"}>button</Button>
+    </div>
+  );
 }
