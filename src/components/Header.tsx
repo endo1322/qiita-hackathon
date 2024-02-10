69dff2555f16074b3f625bda6eb54d3c4899ce24
endo1322 <bondroid3.22@icloud.com>
2024-02-10 23:11:44 +0900

    add: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,82 +40,95 @@ const Header = () => {
     <header
       className={`flex flex-row justify-between bg-white items-center w-full h-[48px] min-h-[48px] px-[30px] shadow-sm`}
     >
-      <Dialog>
-        <DialogTrigger>
-          <JavascriptOriginal className="rounded-md" size={"32px"} />
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>他の技術のコミュニティに移動する</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="flex flex-wrap gap-3">
-            <Link href={`/${locale}/html`}>
-              <Html5Original className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/css3`}>
-              <Css3Original className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/javascript`}>
-              <JavascriptOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/typescript`}>
-              <TypescriptOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/python`}>
-              <PythonOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/go`}>
-              <GoOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/java`}>
-              <JavaOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/kotlin`}>
-              <KotlinOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/swift`}>
-              <SwiftOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/php`}>
-              <PhpOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/c`}>
-              <COriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/cplusplus`}>
-              <CplusplusOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/csharp`}>
-              <CsharpOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/ruby`}>
-              <RubyOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/rust`}>
-              <RustOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/mysql`}>
-              <MysqlOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/postgresql`}>
-              <PostgresqlOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/sqlite`}>
-              <SqliteOriginal className="rounded-md" size={"50px"} />
-            </Link>
-            <Link href={`/${locale}/amazonwebservices`}>
-              <AmazonwebservicesOriginalWordmark
-                className="rounded-md"
-                size={"50px"}
-              />
-            </Link>
-          </div>
-        </DialogContent>
-      </Dialog>
+      <div className="flex gap-5">
+        <button>
+          <Image
+            className=""
+            src="/logo.svg"
+            width={75}
+            height={35}
+            alt="Picture of the author"
+          />
+        </button>
+
+        <Dialog>
+          <DialogTrigger>
+            <JavascriptOriginal className="rounded-md" size={"32px"} />
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>他の技術のコミュニティに移動する</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-wrap gap-3">
+              <Link href={`/${locale}/html`}>
+                <Html5Original className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/css3`}>
+                <Css3Original className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/javascript`}>
+                <JavascriptOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/typescript`}>
+                <TypescriptOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/python`}>
+                <PythonOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/go`}>
+                <GoOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/java`}>
+                <JavaOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/kotlin`}>
+                <KotlinOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/swift`}>
+                <SwiftOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/php`}>
+                <PhpOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/c`}>
+                <COriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/cplusplus`}>
+                <CplusplusOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/csharp`}>
+                <CsharpOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/ruby`}>
+                <RubyOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/rust`}>
+                <RustOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/mysql`}>
+                <MysqlOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/postgresql`}>
+                <PostgresqlOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/sqlite`}>
+                <SqliteOriginal className="rounded-md" size={"50px"} />
+              </Link>
+              <Link href={`/${locale}/amazonwebservices`}>
+                <AmazonwebservicesOriginalWordmark
+                  className="rounded-md"
+                  size={"50px"}
+                />
+              </Link>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+
       <button>
         <Image
           className=" rounded-full"
